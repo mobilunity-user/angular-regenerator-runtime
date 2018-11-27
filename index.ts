398988350f8cafcd58fe25feb5e32a83b81ea922
Mobilunity <o.zhuk@mobilunity.com>
2018-11-27 17:12:39 +0200

    -ашч

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import regeneratorRuntime from "regenerator-runtime";
+import regeneratorRuntime from "regenerator-runtime/runtime";
 
 if ("object" === (typeof window)) {
   (<any>window).regeneratorRuntime = regeneratorRuntime;
